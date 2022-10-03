da16fb9ddece0a8f16c7481cd0bf8c94da0417ed
hantsy <hantsy@gmail.com>
2022-10-03 17:06:01 +0800

    chore(deps): bump to Spring 2.7.4

--- jooq-kotlin-co-gradle/build.gradle.kts
+++ jooq-kotlin-co-gradle/build.gradle.kts
@@ -4,7 +4,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jooq.meta.jaxb.Property
 
 plugins {
-    id("org.springframework.boot") version "2.7.3"
+    id("org.springframework.boot") version "2.7.4"
     id("io.spring.dependency-management") version "1.0.14.RELEASE"
     kotlin("jvm") version "1.7.10"
     kotlin("plugin.spring") version "1.7.10"
