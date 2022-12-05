14db6e82f2466546fe68e9e02c56e19e8d51f417
hantsy <hantsy@gmail.com>
2022-12-05 16:49:02 +0800

    chore: clean codes

--- jooq/src/test/java/com/example/demo/IntegrationTests.java
+++ jooq/src/test/java/com/example/demo/IntegrationTests.java
@@ -3,13 +3,12 @@ package com.example.demo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTests {
@@ -31,8 +30,8 @@ public class IntegrationTests {
         this.webClient.get().uri("/posts")
                 .exchange()
                 .expectStatus().is2xxSuccessful()
-                .expectBody().jsonPath("$[*].title").value((List<String> titles) -> assertThat(titles).containsAnyOf("jooq test"));
+                .expectBody().jsonPath("$[*].title")
+                .value((List<String> titles) -> assertThat(titles).containsAnyOf("jooq test"));
     }
 
 }
-
