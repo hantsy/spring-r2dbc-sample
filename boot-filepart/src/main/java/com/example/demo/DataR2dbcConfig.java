9a6782f4723d748366d779b65dc61319a400c43f
hantsy <hantsy@gmail.com>
2023-01-10 20:34:15 +0800

    chore: clean codes

--- boot-filepart/src/main/java/com/example/demo/DataR2dbcConfig.java
+++ boot-filepart/src/main/java/com/example/demo/DataR2dbcConfig.java
@@ -1,6 +1,7 @@
 package com.example.demo;
 
 
+import io.r2dbc.spi.Blob;
 import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,6 +10,7 @@ import org.springframework.data.convert.ReadingConverter;
 import org.springframework.data.convert.WritingConverter;
 import org.springframework.data.r2dbc.convert.R2dbcCustomConversions;
 import org.springframework.data.r2dbc.dialect.DialectResolver;
+import reactor.core.publisher.Mono;
 
 import java.nio.ByteBuffer;
 import java.util.List;
@@ -16,19 +18,22 @@ import java.util.List;
 @Configuration
 public class DataR2dbcConfig {
 
+    // see: https://github.com/spring-projects/spring-data-relational/issues/1408
     @Bean
     public R2dbcCustomConversions r2dbcCustomConversions(ConnectionFactory connectionFactory) {
         return R2dbcCustomConversions.of(
                 DialectResolver.getDialect(connectionFactory),
                 List.of(
                         new ByteArrayToByteBufferConverter(),
-                        new ByteBufferToByteArrayConverter()
+                        new ByteBufferToByteArrayConverter(),
+                        new ByteArrayToBlobConverter(),
+                        new BlobToByteArrayConverter()
                 )
         );
     }
 }
 
-// see: https://github.com/spring-projects/spring-data-relational/issues/1408
+
 @ReadingConverter
 class ByteArrayToByteBufferConverter implements Converter<byte[], ByteBuffer> {
 
@@ -44,5 +49,23 @@ class ByteBufferToByteArrayConverter implements Converter<ByteBuffer, byte[]> {
     @Override
     public byte[] convert(ByteBuffer source) {
         return source.array();
+    }
+}
+
+@ReadingConverter
+class ByteArrayToBlobConverter implements Converter<byte[], Blob> {
+
+    @Override
+    public Blob convert(byte[] source) {
+        return Blob.from(Mono.just(ByteBuffer.wrap(source)));
+    }
+}
+
+@WritingConverter
+class BlobToByteArrayConverter implements Converter<Blob, byte[]> {
+
+    @Override
+    public byte[] convert(Blob source) {
+        return Mono.from(source.stream()).block().array();
     }
 }
