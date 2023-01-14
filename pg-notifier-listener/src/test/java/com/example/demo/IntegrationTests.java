4e483f4fc1ca09e14627d03ada9fe919cf099be5
hantsy <hantsy@gmail.com>
2023-01-14 13:18:22 +0800

    chore: add tests

--- pg-notifier-listener/src/test/java/com/example/demo/IntegrationTests.java
+++ pg-notifier-listener/src/test/java/com/example/demo/IntegrationTests.java
@@ -4,7 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.test.web.reactive.server.WebTestClient;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.test.StepVerifier;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.time.Duration;
+
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTests {
@@ -12,21 +18,41 @@ public class IntegrationTests {
     @LocalServerPort
     private int port;
 
-    private WebTestClient webClient;
+    private WebClient webClient;
 
     @BeforeEach
     public void setup() {
-        this.webClient = WebTestClient.bindToServer()
+        this.webClient = WebClient.builder()
                 .baseUrl("http://localhost:" + this.port)
                 .build();
     }
 
     @Test
     public void willLoadPosts() {
-        this.webClient.get().uri("/posts")
-                .exchange()
-                .expectStatus().is2xxSuccessful()
-                .expectBodyList(Message.class).hasSize(2);
+        var verifier = webClient.get().uri("messages")
+                .accept(MediaType.TEXT_EVENT_STREAM)
+                .retrieve()
+                .bodyToFlux(Message.class)
+                .log()
+                .as(StepVerifier::create)
+                .consumeNextWith(it -> assertThat(it.body()).isEqualTo("test message"))
+                .consumeNextWith(it -> assertThat(it.body()).isEqualTo("test message2"))
+                .thenCancel()
+                .verifyLater();
+        webClient.post().uri("messages")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new CreateMessageCommand("test message"))
+                .retrieve().toBodilessEntity()
+                .then()
+                .block();
+        webClient.post().uri("messages")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new CreateMessageCommand("test message2"))
+                .retrieve().toBodilessEntity()
+                .then()
+                .block();
+
+        verifier.verify(Duration.ofMillis(500));
     }
 
 }
