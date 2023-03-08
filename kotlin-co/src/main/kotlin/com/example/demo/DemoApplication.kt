a40388febfb658ea2df424009022932ff856276b
hantsy <hantsy@gmail.com>
2023-03-08 21:33:47 +0800

    chore: clean codes

--- kotlin-co/src/main/kotlin/com/example/demo/DemoApplication.kt
+++ kotlin-co/src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,6 +1,9 @@
 package com.example.demo
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.runBlocking
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
@@ -31,6 +34,16 @@ fun main(args: Array<String>) {
 @Configuration
 @EnableR2dbcAuditing
 class DataConfig {
+
+}
+
+@Component
+class SampleDataInitializer(val posts: PostRepository) : ApplicationRunner {
+    override fun run(args: ApplicationArguments?) {
+        runBlocking {
+            posts.save(Post(title = "Sample Post", content = "Sample Content"))
+        }
+    }
 
 }
 
@@ -85,6 +98,7 @@ class PostHandler(val posts: PostRepository) {
                 posts.save(found)
                 return noContent().buildAndAwait()
             }
+
             else -> notFound().buildAndAwait()
         }
     }
