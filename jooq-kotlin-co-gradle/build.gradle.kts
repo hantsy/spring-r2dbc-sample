cab6a94f6e462624cf4ad5477b4fa6b1658cb88b
hantsy <hantsy@gmail.com>
2023-11-24 10:27:56 +0800

    chore: bump to Java 21

--- jooq-kotlin-co-gradle/build.gradle.kts
+++ jooq-kotlin-co-gradle/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_17
+java.sourceCompatibility = JavaVersion.VERSION_21
 
 repositories {
     mavenCentral()
@@ -85,7 +85,7 @@ dependencyManagement {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict", "-opt-in=kotlin.RequiresOptIn")
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
 }
 
