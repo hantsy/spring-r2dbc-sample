689918591fbb27624de5d3b9e7ed240352035147
hantsy <hantsy@gmail.com>
2023-12-18 15:17:26 +0800

    chore: add an example entity to refer itself

--- jooq-kotlin-co-gradle/src/main/resources/schema.sql
+++ jooq-kotlin-co-gradle/src/main/resources/schema.sql
@@ -18,6 +18,17 @@ CREATE TABLE IF NOT EXISTS comments (
     created_at TIMESTAMP
 );
 
+
+CREATE TABLE IF NOT EXISTS nodes (
+    id UUID NOT NULL /* [jooq ignore start] */ DEFAULT uuid_generate_v4() /* [jooq ignore stop] */,
+    name VARCHAR(50),
+    description VARCHAR(255),
+    parent_id UUID
+);
+
+
 ALTER TABLE posts ADD CONSTRAINT posts_pk PRIMARY KEY (id);
 ALTER TABLE comments ADD CONSTRAINT comments_pk PRIMARY KEY (id);
+ALTER TABLE nodes ADD CONSTRAINT nodes_pk PRIMARY KEY (id);
 ALTER TABLE comments ADD CONSTRAINT comments_posts_fk FOREIGN KEY (post_id) REFERENCES posts(id) /* [jooq ignore start] */ON DELETE CASCADE ON UPDATE CASCADE/* [jooq ignore stop] */;
+ALTER TABLE nodes ADD CONSTRAINT nodes_parent_fk FOREIGN KEY (parent_id) REFERENCES nodes(id) /* [jooq ignore start] */ON UPDATE CASCADE/* [jooq ignore stop] */;
