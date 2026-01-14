0fd2ea0d5f531e34a7c1c07e0337ec2d6aa972f6
hantsy <hantsy@gmail.com>
2026-01-15 00:16:19 +0800

    chore: clean codes

--- data-r2dbc-repositories/src/main/java/com/example/demo/Application.java
+++ data-r2dbc-repositories/src/main/java/com/example/demo/Application.java
@@ -1,34 +1,30 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.*;
-import org.springframework.http.server.reactive.HttpHandler;
-import org.springframework.http.server.reactive.ReactorHttpHandlerAdapter;
-import org.springframework.web.server.adapter.WebHttpHandlerBuilder;
-import reactor.netty.http.server.HttpServer;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 @Configuration
 @ComponentScan
 @PropertySource(value = "classpath:application.properties", ignoreResourceNotFound = true)
+@Slf4j
 public class Application {
 
     @Value("${server.port:8080}")
     private int port = 8080;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         try (AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
                 Application.class)) {
-            context.getBean(HttpServer.class).bindNow().onDispose().block();
+            var posts = context.getBean(PostRepository.class);
+            posts.findAll()
+                    .subscribe(
+                            data -> log.debug("found post:{}", data),
+                            throwable -> log.debug("error caught:{}", throwable.getMessage())
+                    );
         }
-    }
-
-    @Profile("default")
-    @Bean
-    public HttpServer nettyHttpServer(ApplicationContext context) {
-        HttpHandler handler = WebHttpHandlerBuilder.applicationContext(context).build();
-        ReactorHttpHandlerAdapter adapter = new ReactorHttpHandlerAdapter(handler);
-        HttpServer httpServer = HttpServer.create().host("localhost").port(this.port);
-        return httpServer.handle(adapter);
     }
 }
